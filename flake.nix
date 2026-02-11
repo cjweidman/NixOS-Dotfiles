2c840b6af814948f6bb53f68916b9d6ce8f92b31
Caleb Weidman <caleb@blk.bar>
2026-02-11 15:56:35 -0500

    First Commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "aarch64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      cjweidman = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs; 
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+
+}
